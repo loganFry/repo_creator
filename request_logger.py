650fe1a0cd9e14cb32d6ba7e8420616348138e55
Logan Fry <loganfrybball@yahoo.com>
2018-07-09 21:52:36 -0400

    Create class to handle logging HTTP requests

---
+++ request_logger.py
@@ -0,0 +1,27 @@
+import json
+import requests
+
+class RequestLogger:
+    """ Class to log details about HTTP requests """
+
+    def __init__(self, indent=4):        
+        self.updateIndent(indent)
+
+    def updateIndent(self, newValue):
+        self.indent = newValue
+        if newValue > 0:
+            self.indentToken = ""
+            for i in range(1, newValue):
+                self.indentToken += " "
+        else:
+            self.indentToken = ""            
+
+    def log(self, request):
+        print("Response headers: ")
+        print("{")
+        for header, val in request.headers.items():
+            print(self.indentToken + header + ": " + val)                    
+        print("}")
+
+        print("Response body: ")
+        print(json.dumps(request.json(), indent=self.indent))
