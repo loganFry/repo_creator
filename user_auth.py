4ed7fa172aaad30eff82d5509351c13fb7d7891f
Logan Fry <loganfrybball@yahoo.com>
2018-07-09 20:18:47 -0400

    Create class to handle a user's auth info

---
+++ user_auth.py
@@ -0,0 +1,23 @@
+import json
+
+class UserAuth:
+    """ Class to contain all authentication information for a user """
+
+    def __init__(self):
+        self.username = None
+        self.token = None
+
+    def load(self, filename):
+        try:
+            with open(filename) as current_file:
+                file_data = json.load(current_file)
+                self.username = file_data["username"]
+                self.token = file_data["token"]
+        except FileNotFoundError:
+            print("File does not exist")
+
+    def is_valid(self):
+        valid = self.username is not None and self.token is not None
+        return valid
+
+
