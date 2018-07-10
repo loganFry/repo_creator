54e4ffba4ae68bd2c539442d484d993f0575d59f
Logan Fry <loganfrybball@yahoo.com>
2018-07-09 20:18:21 -0400

    Reorganize, add comments

--- main.py
+++ main.py
@@ -3,23 +3,35 @@ import getpass
 from user_auth import UserAuth
 from api import Api
 
+# Config
 user_data_filename = "user_data.txt"
-user_data = UserAuth()
-user_data.load(user_data_filename)
 base_url = "https://api.github.com"
 
+# Create initial model of user authentication from file
+user_data = UserAuth()
+user_data.load(user_data_filename)
+
+# If the cached user credentials are valid, use them
 if user_data.is_valid():
     print("Read cached user authentication from " + user_data_filename)
     print("Username: " + user_data.username)
     print("Oauth token: " + user_data.token)
+# Otherwise get info from the user to create a new access token
 else:
     username = input("Enter your Github username: ")
-    password = getpass.getpass("Enter your Github password: ")    
+    password = getpass.getpass("Enter your Github password: ")   
+
+    # Make api call to create an access token
     api = Api()
     print("Issuing request to create an Oauth token...")
     token_json = api.create_token(username, password, debug=False)
     print("Success! Your authorization has succeeded")
+
+    # Save the created token in memory to continue using it
     user_data.token = token_json["token"]
+
+    # Add the entered username to the token dictionary and save the whole thing
+    # to a file
     token_json["username"] = username
     with open(user_data_filename, 'w') as user_file:
         json.dump(token_json, user_file)
